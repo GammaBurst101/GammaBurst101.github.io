078464542bdda882a825ec60c1e9a59b627e9a33
Prasoon <prasoonpandey06@gmail.com>
2019-03-31 18:27:42 +0530

    Change the canvas size

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(480, 320, Phaser.CANVAS, null, {preload: preload, create: create, update: update});
+var game = new Phaser.Game(480 * 1.5, 320 * 1.5, Phaser.CANVAS, null, {preload: preload, create: create, update: update});
 var ball;
 var paddle;
 var bricks;
@@ -12,13 +12,12 @@ var lifeLostText;
 var textStyle = { font: '18px Arial', fill: '#0095DD' };
 var playing = false;
 var startButton;
-	
+
 function preload(){
-	game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 	game.scale.pageAlignHorizontally = true;
 	game.scale.pageAlignVertically = true;
 	game.stage.backgroundColor = "#eee";
-	
+
 	//Load the assets required for the game
 	game.load.image('ball', 'images/ball.png');
 	game.load.image('paddle', 'images/paddle.png');
@@ -35,19 +34,19 @@ function create() {
 	game.physics.enable(ball, Phaser.Physics.ARCADE);
 	ball.body.collideWorldBounds = true;
 	ball.body.bounce.set(1);
-	
+
 	game.physics.arcade.checkCollision.down = false;
 	ball.checkWorldBounds = true;
 	ball.events.onOutOfBounds.add (ballLeaveScreen, this);
-	
+
 	//Paddle
 	paddle = game.add.sprite(game.world.width*0.5, game.world.height-5, 'paddle');
 	paddle.anchor.set(0.5, 1);
 	game.physics.enable(paddle, Phaser.Physics.ARCADE);
 	paddle.body.immovable = true;
-	
+
 	initBricks();
-	
+
 	//Labels
 	scoreText = game.add.text( 5, 5, 'Points: 0', textStyle);
 	livesText = game.add.text(game.world.width - 5, 5, 'Lives: '+lives, textStyle);
@@ -55,7 +54,7 @@ function create() {
 	lifeLostText = game.add.text(game.world.width*0.5, game.world.height*0.5, 'Life lost, click to continue', textStyle);
 	lifeLostText.anchor.set(0.5);
 	lifeLostText.visible = false;
-	
+
 	//Start Button
 	startButton = game.add.button(game.world.width * 0.5, game.world.height * 0.5, 'button', startGame, this, 1, 0, 2);
 	startButton.anchor.set(0.5);
@@ -64,7 +63,7 @@ function create() {
 function update() {
 	game.physics.arcade.collide(ball, paddle, ballHitPaddle);
 	game.physics.arcade.collide(ball, bricks, ballHitBrick);
-	
+
 	if (playing)
 		paddle.x = game.input.x || game.world.width*0.5;
 }
@@ -83,9 +82,9 @@ function initBricks() {
 		},
 		padding: 10
 	};
-	
+
 	bricks = game.add.group();
-	
+
 	for (c = 0; c < brickInfo.count.col; c++) {
 		for (r = 0; r < brickInfo.count.row; r++) {
 			brickX = ( c * (brickInfo.width + brickInfo.padding)) + brickInfo.offset.left;
@@ -100,24 +99,24 @@ function initBricks() {
 }
 
 function ballHitBrick (ball, brick) {
-	//Tweening the bricks to ease out 
+	//Tweening the bricks to ease out
 	var killTween = game.add.tween(brick.scale);
 	killTween.to({x: 0, y: 0}, 200, Phaser.Easing.Linear.None);
 	killTween.onComplete.addOnce( function () {
 		brick.kill();
 	}, this);
 	killTween.start();
-	
+
 	score += 10;
 	scoreText.setText('Points: '+score);
-	
+
 	//As of 11.13.19, the following code counts 1 extra brick every time so the initial brick count is set to -1 (temp fix)
 	var bricksLeft = -1;
 	for (i = 0; i < bricks.children.length; i++) {
 		if (bricks.children[i].alive == true)
 			bricksLeft ++;
 	}
-	
+
 	if (bricksLeft == 0) {
 		alert('You won the game, congratulations!');
 		location.reload();
@@ -152,15 +151,3 @@ function startGame() {
 	ball.body.velocity.set(150, -150);
 	playing = true;
 }
-
-
-
-
-
-
-
-
-
-
-
-
